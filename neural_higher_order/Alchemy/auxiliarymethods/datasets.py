94d86d79102f71d2bd4453de73e5e80a548122b0
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-06-10 21:24:51 -0400

    Added neural baselines.

---
+++ neural_higher_order/Alchemy/auxiliarymethods/datasets.py
@@ -0,0 +1,44 @@
+import os.path as osp
+
+import numpy as np
+from torch_geometric.datasets import TUDataset
+
+
+# Return classes as numpy array.
+def read_classes(ds_name):
+    # Classes
+    with open("datasets/" + ds_name + "/raw/" + ds_name + "_graph_labels.txt", "r") as f:
+        classes = [int(i) for i in list(f)]
+    f.closed
+
+    return np.array(classes)
+
+
+def read_targets(ds_name):
+    # Classes
+    with open("datasets/" + ds_name + "/" + ds_name + "/raw/" + ds_name + "_graph_attributes.txt", "r") as f:
+        classes = [float(i) for i in list(f)]
+    f.closed
+
+    return np.array(classes)
+
+
+def read_multi_targets(ds_name):
+    # Classes
+    with open("datasets/" + ds_name + "/" + ds_name + "/raw/" + ds_name + "_graph_attributes.txt", "r") as f:
+        classes = [[float(j) for j in i.split(",")] for i in list(f)]
+    f.closed
+
+    return np.array(classes)
+
+
+def get_dataset(dataset, regression=False, multigregression=False):
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'datasets', dataset)
+    TUDataset(path, name=dataset)
+
+    if multigregression:
+        return read_multi_targets(dataset)
+    if not regression:
+        return read_classes(dataset)
+    else:
+        return read_targets(dataset)
