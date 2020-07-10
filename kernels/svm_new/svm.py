76a900cad4b5a6b72c137ff22f0cdbe3908ad8d1
Christopher Morris <christopher.morris@udo.edu>
2020-07-10 17:41:02 -0400

    New svm.

--- kernels/svm_new/svm.py
+++ kernels/svm_new/svm.py
@@ -23,18 +23,19 @@ def main():
 
     dataset = [["IMDB-BINARY", False], ["IMDB-MULTI", False], ["NCI1", True], ["NCI109", True], ["PROTEINS", True],
                ["PTC_FM", True], ["REDDIT-BINARY", False], ["ENZYMES", True]]
-    algorithm = "LWLP2"
+    algorithms = ["LWLP2", "LWLP3"]
 
-    for d, use_labels in dataset:
-        gram_matrices = []
-        for i in range(0,6):
-            gram_matrix, _ = read_lib_svm(path + d + "__" + algorithm + "_" + str(i) + ".gram")
-            classes = read_classes(d)
-            gram_matrices.append(gram_matrix)
+    for a in algorithms:
+        for d, use_labels in dataset:
+            gram_matrices = []
+            for i in range(0,6):
+                gram_matrix, _ = read_lib_svm(path + d + "__" + a + "_" + str(i) + ".gram")
+                classes = read_classes(d)
+                gram_matrices.append(gram_matrix)
 
 
-        acc, s_1, s_2 = kernel_svm_evaluation(gram_matrices, classes, num_repetitions=10, all_std=True)
-        print(acc, s_1, s_2)
+            acc, s_1, s_2 = kernel_svm_evaluation(gram_matrices, classes, num_repetitions=10, all_std=True)
+            print(acc, s_1, s_2)
 
 
 
