f4c66c8d59ef9450733976f4a0f15e24146c1a47
Christopher Morris <christopher.morris@udo.edu>
2020-08-14 00:36:32 -0400

    New svm.

--- kernels/svm_new/svm.py
+++ kernels/svm_new/svm.py
@@ -50,7 +50,7 @@ def main():
             # Collect feature matrices over all iterations
             all_feature_matrices = []
             classes = read_classes(name)
-            for i in range(0, 6):
+            for i in range(2, 3):
                 # Load feature matrices.
                 feature_vector = pd.read_csv(path + name + "__" + algorithm + "_" + str(i), header=1,
                                              delimiter=" ").to_numpy()
