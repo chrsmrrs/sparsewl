260a2fcd91c4af731e5dbe173af20363d9f01d59
Christopher Morris <christopher.morris@udo.edu>
2020-10-20 19:40:31 -0400

    New svm.

--- kernels/main.cpp
+++ kernels/main.cpp
@@ -27,52 +27,6 @@ using namespace std;
 int main() {
     // k = 2.
     {
-
-
-
-
-        vector<pair<string, bool>> datasets = {make_pair("IMDB-BINARY", false),make_pair("ENZYMES", true),
-                                               make_pair("IMDB-MULTI", false), make_pair("NCI1", true),
-                                               make_pair("PTC_FM", true),
-                                               make_pair("PROTEINS", true)};
-
-
-        for (auto &d: datasets) {
-            {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
-                string kernel = "LWLC2";
-                GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
-                gdb.erase(gdb.begin() + 0);
-                vector<int> classes = AuxiliaryMethods::read_classes(ds);
-
-                GenerateTwo::GenerateTwo wl(gdb);
-                for (uint i = 0; i <= 10; ++i) {
-                    cout << ds + "__" + kernel + "_" + to_string(i) << endl;
-                    GramMatrix gm;
-
-                    if (i == 5) {
-                        high_resolution_clock::time_point t1 = high_resolution_clock::now();
-                        gm = wl.compute_gram_matrix(i, use_labels, false, "localc", true, true);
-                        high_resolution_clock::time_point t2 = high_resolution_clock::now();
-                        auto duration = duration_cast<seconds>(t2 - t1).count();
-                        cout << duration << endl;
-                    } else {
-                        gm = wl.compute_gram_matrix(i, use_labels, false, "localc", true, true);
-                    }
-
-                    AuxiliaryMethods::write_libsvm(gm, classes,
-                                                   "./svm/GM/EXP/" + ds + "__" + kernel + "_" + to_string(i) +
-                                                   ".gram");
-                }
-            }
-        }
-    }
-
-    /*
-    // k = 2.
-    {
         vector<pair<string, bool>> datasets = {make_pair("ENZYMES", true), make_pair("IMDB-BINARY", false),
                                                make_pair("IMDB-MULTI", false), make_pair("NCI1", true),
                                                make_pair("NCI109", true), make_pair("PTC_FM", true),
@@ -549,6 +503,6 @@ int main() {
             }
         }
     }
-*/
+
     return 0;
 }
