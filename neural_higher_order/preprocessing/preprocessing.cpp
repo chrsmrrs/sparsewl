c44e1618192507ad5151be7254de89aec24a0dfc
Christopher Morris <christopher.morris@udo.edu>
2021-04-10 19:34:18 -0400

    New Experiments.

--- neural_higher_order/preprocessing/preprocessing.cpp
+++ neural_higher_order/preprocessing/preprocessing.cpp
@@ -2824,7 +2824,7 @@ PYBIND11_MODULE(preprocessing, m) {
     m.def("get_all_node_labels_ZINC_1", &get_all_node_labels_ZINC_1);
     m.def("get_all_node_labels_alchem_1", &get_all_node_labels_alchem_1);
     m.def("get_all_node_labels_allchem", &get_all_node_labels_allchem);
-    m.def("get_all_node_labels_allchem_connected", &get_all_node_labels_allchem_connected);
+    //m.def("get_all_node_labels_allchem_connected", &get_all_node_labels_allchem_connected);
 
     m.def("get_all_node_labels_allchem_both", &get_all_node_labels_allchem_both);
 
