f3d61209d6e278e79378f18dbdf76791e1581751
Christopher Morris <christopher.morris@udo.edu>
2021-04-11 13:33:30 -0400

    New Experiments.

--- neural_higher_order/Alchemy/local_2_10K_both.py
+++ neural_higher_order/Alchemy/local_2_10K_both.py
@@ -236,10 +236,8 @@ class NetGIN(torch.nn.Module):
         x = x_6_r
 
 
-        print(data.batch_unc.type())
-        exit()
 
-        x = self.set2set(x, data.batch_unc)
+        x = self.set2set(x, data.batch_unc.to(torch.long))
 
         x = F.relu(self.fc1(x))
         x = self.fc4(x)
