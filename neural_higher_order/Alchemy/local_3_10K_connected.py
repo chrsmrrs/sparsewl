0aac09dc27ff39b7f91a95b144b22c2ab62762b6
Christopher Morris <christopher.morris@udo.edu>
2021-04-13 19:59:48 -0400

    New Experiments.

--- neural_higher_order/Alchemy/local_3_10K_connected.py
+++ neural_higher_order/Alchemy/local_3_10K_connected.py
@@ -253,7 +253,7 @@ results = []
 results_log = []
 for _ in range(5):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = NetGIN(10).to(device)
+    model = NetGIN(20).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
                                                            factor=0.5, patience=5,
